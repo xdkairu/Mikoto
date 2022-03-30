cf1fb0e5ff7dd8e302a02fb96969f6814f1a3e3b
Thatskylee <justavalonn@gmail.com>
2022-03-30 04:15:52 +0100

    Added Black Clover Episode Search

--- src/main/java/org/Clover/Fun/BCEpisodes.java
+++ src/main/java/org/Clover/Fun/BCEpisodes.java
@@ -12,8 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class BCEpisodes extends ListenerAdapter {
 
-    private static final String MESSAGE = "**Title**: `%s`\n**Episode**: `%s`\n**Link**: [MyAnimeList](%s \"Black Clover Episode Link\")";
-    private static final String ERROR_MESSAGE = "Invalid episode number!";
+    private static final String MESSAGE = "**Title**: `%s`\n**Episode**: `%s`\n**Duration**: `%s`\n**Link**: [CrunchyRoll](%s \"Black Clover Episode Link\")";
     private final Clover clover;
     Data data = new Data();
 
@@ -29,9 +28,9 @@ public class BCEpisodes extends ListenerAdapter {
         int number;
         try {
             number = event.getOption("episode").getAsInt();
-        } catch (NullPointerException e) {
+        } catch (NullPointerException | NumberFormatException e) {
             EmbedBuilder eb = new EmbedBuilder();
-            eb.setDescription("`" + event.getOption("episode") + "` is not an episode from Black Clover");
+            eb.setDescription("`" + event.getOption("episode").getAsString() + "` is not an episode from Black Clover");
             eb.setColor(new Color(Data.failedRed));
             eb.setTimestamp(Instant.now());
             eb.setFooter("Invalid Episode Number", data.getSelfAvatar(event));
@@ -46,13 +45,22 @@ public class BCEpisodes extends ListenerAdapter {
         var episode = clover.getEpisodes().get(number);
 
         if (episode == null) {
-            event.reply(ERROR_MESSAGE).setEphemeral(true).queue();
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setDescription("`" + event.getOption("episode").getAsString() + "` is not an episode from Black Clover");
+            eb.setColor(new Color(Data.failedRed));
+            eb.setTimestamp(Instant.now());
+            eb.setFooter("Invalid Episode Number", data.getSelfAvatar(event));
+
+            event.replyEmbeds(eb.build()).queue((msg) -> {
+                eb.clear();
+                msg.deleteOriginal().queueAfter(10, TimeUnit.SECONDS);
+            });
             return;
         }
 
         var embed = new EmbedBuilder();
         embed.setThumbnail("https://i.imgur.com/WLKozg4.jpeg");
-        embed.setDescription(String.format(MESSAGE, episode.name(), number, episode.url()));
+        embed.setDescription(String.format(MESSAGE, episode.name(), number, "23:53", episode.url()));
         embed.setColor(new Color(data.successGreen));
         embed.setTimestamp(Instant.now());
         embed.setFooter("Black Clover Episode", data.getSelfAvatar(event));
